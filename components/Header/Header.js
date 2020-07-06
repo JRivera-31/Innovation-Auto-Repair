987aaa3f7d4f106d42e43472b2e106f55a41dd5f
Grayson Deese <gldeese@ncsu.edu>
2020-07-06 12:11:18 -0400

    Header finished

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,12 @@
-import "./module.css"
+import headerStyles from "./header.module.css"
 function Header() {
     return (
-        <div className="header">
-        <div className="innerHeader">
-          <div className="logoContainer">
+        <div className={headerStyles.header}>
+        <div className={headerStyles.innerHeader}>
+          <div className={headerStyles.logoContainer}>
             <h1>Innovation Auto Hail Repair</h1>
           </div>
-          <ul className="navigation">
+          <ul className={headerStyles.navigation}>
             <a><li>About</li></a>
             <a><li>Contact</li></a>
             <a><li>Book With Us</li></a>

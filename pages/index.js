bf0855b7378187dd130f7da58f3d431a981f9291
Josh R <jriver35@uncc.edu>
2020-07-06 13:49:04 -0400

    removed layout from index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Header from "../components/Header/Header";
 import Head from "next/head"
 import Hero from '../components/Hero/Hero';
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <div>
-      <Header />
-      <Hero />
+        <Header />
+        <Hero />
     </div>
   )
 }

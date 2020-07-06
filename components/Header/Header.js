405cb98676fd20a4c32f81f930ba692b2031fdf7
Grayson Deese <gldeese@ncsu.edu>
2020-07-06 14:20:57 -0400

    sidebar displaying

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,8 @@
 import headerStyles from "./header.module.css"
 function Header() {
     return (
+      <div>
+        
         <div className={headerStyles.header}>
         <div className={headerStyles.innerHeader}>
           <div className={headerStyles.logoContainer}>
@@ -12,6 +14,29 @@ function Header() {
             <a><li>Book With Us</li></a>
           </ul>
         </div>
+      </div>
+
+
+      <nav className={headerStyles.sidebar}>
+        <ul className={headerStyles.navList}>
+          <li className={headerStyles.navItem}>
+            <a className={headerStyles.navLink}
+              >About</a
+            >
+          </li>
+          <li className={headerStyles.navItem}>
+            <a
+              className={headerStyles.navLink}
+              >Contact</a
+            >
+          </li>
+          <li className={headerStyles.navItem}>
+            <a className={headerStyles.navLink}
+              >Book With Us</a
+            >
+          </li>
+        </ul>
+      </nav>
       </div>
     );
   }

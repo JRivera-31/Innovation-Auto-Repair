bfd166dabd817d331ad4754ea1a6956cbe846c8f
Emily Renegar <egrenegar@gmail.com>
2020-07-06 14:17:16 -0400

    added ourTeam component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header/Header";
+import OurTeam from "../components/OurTeam/ourTeam";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <OurTeam/>
     </div>
   )
 }

0114c8074cfc569effd37c149e675e79773e45d1
Grayson Deese <gldeese@ncsu.edu>
2020-07-06 11:46:03 -0400

    Header component started

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+
+function Header() {
+    return (
+        <div className="header">
+        <div className="innerHeader">
+          <div className="logoContainer">
+            <h1>my<span>SITE</span></h1>
+          </div>
+          <ul className="navigation">
+            <a><li>About</li></a>
+            <a><li>Contact</li></a>
+            <a><li>Book With Us</li></a>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Header;
+  

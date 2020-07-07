bcbbf764fe6ec6b7455162806b4947340e7d94dc
Grayson Deese <gldeese@ncsu.edu>
2020-07-07 13:27:34 -0400

    Logo in header

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
 import Header from "../components/Header/Header";
-import Hero from '../components/Hero/Hero';
-import OurTeam from '../components/OurTeam/ourTeam';
-import About from "../components/About/about"
-import Form from "../components/ContactForm/contact"
+import Hero from "../components/Hero/Hero";
+import OurTeam from "../components/OurTeam/ourTeam";
+import About from "../components/About/about";
+import Form from "../components/ContactForm/contact";
 
 export default function Home() {
   return (
-    <html>
+    <html lang="en">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Innovation Auto</title>
-        <link href="https://fonts.googleapis.com/css2?family=Chau+Philomene+One&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Chau+Philomene+One&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          rel="shortcut icon"
+          type="img/png"
+          href="../public/images/Innovation_Logo-no-bg.png"
+        />
       </head>
       <body>
         <Header />
@@ -20,5 +33,5 @@ export default function Home() {
         <Form />
       </body>
     </html>
-  )
+  );
 }

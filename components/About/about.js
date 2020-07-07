ac347ee20c3c08f2b41540105e47b7b67b6792d1
Emily Renegar <egrenegar@gmail.com>
2020-07-07 15:20:17 -0400

    div around <h1> and <hr>, split body text into two <p> tags

--- components/About/about.js
+++ components/About/about.js
@@ -3,15 +3,15 @@ import styles from "./about.module.css";
 export default function About() {
     return (
         <div>
-            <div>
-                <h1 className={styles.header}>About</h1>
+            <div className={styles.header}>
+                <h1 >About</h1>
                 <hr className={styles.aboutHr}/>
             </div>
             <div className={styles.container}>
                 <div className={styles.information}>
-                    Innovation Auto Hail Repair works to restore your vehicle to its pre-storm condition. We work to bring back the value to your car. 
-                    With experienced technicians working on your vehicle we can guarantee your vehicle is in good hands. Don’t have insurance? 
-                    No problem! We give special discounts for those paying out of pocket. Have a non-hail related dent? We fix those too! Contact us for any questions or concerns!   
+                    Innovation Auto Hail Repair works to restore your vehicle to its pre-storm condition. We work to bring back the value to your car. With experienced technicians working on your vehicle we can guarantee your vehicle is in good hands. 
+                   <br/><br/>
+                    Don’t have insurance? No problem! We give special discounts for those paying out of pocket. Have a non-hail related dent? We fix those too! Contact us for any questions or concerns! 
                 </div>
                 <div className={styles.imageDiv}>
                     <img className={styles.image} src='/images/car.jpg' />  

e27492c5a822cc84d9eb95925a687653dd900d2f
Grayson Deese <gldeese@ncsu.edu>
2020-07-06 21:12:29 -0400

    Hamburger displaying

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,13 +1,32 @@
 import headerStyles from "./header.module.css"
 function Header() {
+  
+    const sidebar = document.querySelector(".sidebar");
+    const line1 = document.querySelector(".line-1"); 
+    const line2 = document.querySelector(".line-2");
+    const line3 = document.querySelector(".line-3"); 
+  const click = () => {
+    sidebar.classList.toggle('change');
+    line1.classList.toggle('rotate1');
+    line2.classList.toggle('rotate2');
+    line3.classList.toggle('rotate3');
+      }
+
     return (
       <div>
-        
         <div className={headerStyles.header}>
         <div className={headerStyles.innerHeader}>
           <div className={headerStyles.logoContainer}>
             <h1>Innovation Auto Hail Repair</h1>
           </div>
+          <div className={headerStyles.navHolder}>
+                <div onClick={click} className={headerStyles.hamburgerMenu}>
+                  <div className={`${headerStyles.line} ${headerStyles.line-1}`}></div>
+                  <div className={`${headerStyles.line} ${headerStyles.line-2}`}></div>
+                  <div className={`${headerStyles.line} ${headerStyles.line-3}`}></div>
+                </div>
+              </div>
+
           <ul className={headerStyles.navigation}>
             <a><li>About</li></a>
             <a><li>Contact</li></a>
@@ -38,6 +57,7 @@ function Header() {
         </ul>
       </nav>
       </div>
+      
     );
   }
   

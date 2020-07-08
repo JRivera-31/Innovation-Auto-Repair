7a8ba60bfab87562f3475eabf1bcbad364b24985
Josh R <jriver35@uncc.edu>
2020-07-08 10:01:12 -0400

    tried to make footer stick to bottom, not working

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           <About />
           <OurTeam />
           <Form />
-          <Footer/>
+          <Footer />
           </div>
         </div>
       </body>

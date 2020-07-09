9cda6269cfe6749bc952668cf6e71a8676343fdb
Emily Renegar <egrenegar@gmail.com>
2020-07-08 21:38:55 -0400

    edited modal

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -66,8 +66,8 @@ function Header() {
       <div className={headerStyles.header}>
         <div className={headerStyles.topHeader}> 
         <div className={headerStyles.modalText}>
-          <p>Refer a friend, Get $200. {""}
-  <span onClick={handleOpen}>Learn more</span>
+          <p>Refer a friend, get $200. {""}
+  <span onClick={handleOpen}>Learn more.</span>
           </p>
           </div>
           </div>
@@ -89,7 +89,15 @@ function Header() {
         <Fade in={open}>
           <div className={classes.paper}>
             <h2 id="transition-modal-title">Our Referral Program:</h2>
-            <p id="transition-modal-description">Refer someone to us, if they get their car fixed with us, you get $200.</p>
+            <div id="transition-modal-description">
+              <br/>
+              <p>
+                Refer a friend and get $200 if they book with us.
+              </p>
+              <p>
+                Just ask them to mention your name to one of our staff!
+              </p>
+            </div>
           </div>
         </Fade>
       </Modal>

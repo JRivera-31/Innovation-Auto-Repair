2c4dcf0bb6202441e7a552ce7437c41b01d104f1
Emily Renegar <egrenegar@gmail.com>
2020-07-08 20:39:41 -0400

    adding BookingDetails component

---
+++ components/BookingDetails/details.js
@@ -0,0 +1,12 @@
+import styles from './details.module.css';
+
+export default function Details() {
+    <div>
+        <h3>
+            Details:
+        </h3>
+        <p>
+            Each appointment includes a free full detailing!
+        </p>
+    </div>
+};

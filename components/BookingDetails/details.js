cb439e0b4a06238938cd1f969a6c9cd1e888a513
Emily Renegar <egrenegar@gmail.com>
2020-07-09 20:23:45 -0400

    Details displaying some dummy text

--- components/BookingDetails/details.js
+++ components/BookingDetails/details.js
@@ -1,12 +1,14 @@
 import styles from './details.module.css';
 
 export default function Details() {
-    <div className={styles.container}>
-        <h3 className={styles.header}>
-            Details:
-        </h3>
-        <p>
-            Each appointment includes a free full detailing!
-        </p>
-    </div>
+    return(
+        <section className={styles.container}>
+            <h3 className={styles.header}>
+                Details:
+            </h3>
+            <p>
+               HELLLOOOOOO!!
+            </p>
+        </section>
+    );
 };

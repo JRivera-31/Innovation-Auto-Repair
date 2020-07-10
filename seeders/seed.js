9cefa1fe0f0f49f1644ae4a11bcad56e68434b3e
Josh R <jriver35@uncc.edu>
2020-07-10 13:32:11 -0400

    re did schema and seeds

--- seeders/seed.js
+++ seeders/seed.js
@@ -5,111 +5,19 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/Innovatio
   useNewUrlParser: true
 })
 
-let availableTimeSeed = [{
-    availableTimes: [
-        {
-            day: 0,
-            times: "No available times"
-        },
-        {
-            day: 1,
-            times: [
-                {time: "9:00", isAvailable: true},
-                {time: "9:30", isAvailable: true},
-                {time: "10:00", isAvailable: true},
-                {time: "10:30", isAvailable: true},
-                {time: "11:00", isAvailable: true},
-                {time: "11:30", isAvailable: true},
-                {time: "1:00", isAvailable: true},
-                {time: "1:30", isAvailable: true},
-                {time: "2:00", isAvailable: true},
-                {time: "2:30", isAvailable: true},
-                {time: "3:00", isAvailable: true},
-                {time: "3:30", isAvailable: true},
-                {time: "4:00", isAvailable: true},
-                {time: "4:30", isAvailable: true}]
-        },
-        {
-            day: 2,
-            times: [
-                {time: "9:00", isAvailable: true},
-                {time: "9:30", isAvailable: true},
-                {time: "10:00", isAvailable: true},
-                {time: "10:30", isAvailable: true},
-                {time: "11:00", isAvailable: true},
-                {time: "11:30", isAvailable: true},
-                {time: "1:00", isAvailable: true},
-                {time: "1:30", isAvailable: true},
-                {time: "2:00", isAvailable: true},
-                {time: "2:30", isAvailable: true},
-                {time: "3:00", isAvailable: true},
-                {time: "3:30", isAvailable: true},
-                {time: "4:00", isAvailable: true},
-                {time: "4:30", isAvailable: true}]
-        },
-        {
-            day: 3,
-            times: [
-                {time: "9:00", isAvailable: true},
-                {time: "9:30", isAvailable: true},
-                {time: "10:00", isAvailable: true},
-                {time: "10:30", isAvailable: true},
-                {time: "11:00", isAvailable: true},
-                {time: "11:30", isAvailable: true},
-                {time: "1:00", isAvailable: true},
-                {time: "1:30", isAvailable: true},
-                {time: "2:00", isAvailable: true},
-                {time: "2:30", isAvailable: true},
-                {time: "3:00", isAvailable: true},
-                {time: "3:30", isAvailable: true},
-                {time: "4:00", isAvailable: true},
-                {time: "4:30", isAvailable: true}]
-        },
-        {
-            day: 4,
-            times: [
-                {time: "9:00", isAvailable: true},
-                {time: "9:30", isAvailable: true},
-                {time: "10:00", isAvailable: true},
-                {time: "10:30", isAvailable: true},
-                {time: "11:00", isAvailable: true},
-                {time: "11:30", isAvailable: true},
-                {time: "1:00", isAvailable: true},
-                {time: "1:30", isAvailable: true},
-                {time: "2:00", isAvailable: true},
-                {time: "2:30", isAvailable: true},
-                {time: "3:00", isAvailable: true},
-                {time: "3:30", isAvailable: true},
-                {time: "4:00", isAvailable: true},
-                {time: "4:30", isAvailable: true}]
-        },
-        {
-            day: 5,
-            times: [
-                {time: "9:00", isAvailable: true},
-                {time: "9:30", isAvailable: true},
-                {time: "10:00", isAvailable: true},
-                {time: "10:30", isAvailable: true},
-                {time: "11:00", isAvailable: true},
-                {time: "11:30", isAvailable: true},
-                {time: "1:00", isAvailable: true},
-                {time: "1:30", isAvailable: true},
-                {time: "2:00", isAvailable: true},
-                {time: "2:30", isAvailable: true},
-                {time: "3:00", isAvailable: true},
-                {time: "3:30", isAvailable: true},
-                {time: "4:00", isAvailable: true},
-                {time: "4:30", isAvailable: true}]
-        },
-        {
-            day: 6,
-            times: "No available times"
-        }
-    ]
-}]
+let appointmentSeeds = [
+    {
+        name: "Test Tester",
+        email: "test@test.com",
+        phonenumer: "555 555 5555",
+        description: "dfasfasfasdf",
+        dateAndTime: "1/1/20 9:30"
+    }
+]
+
 
 db.Appointments.deleteMany({})
-    .then(() => db.Appointments.collection.insertMany(availableTimeSeed))
+    .then(() => db.Appointments.collection.insertMany(appointmentSeeds))
     .then(data => {
         console.log(data.result.n)
         process.exit(0)

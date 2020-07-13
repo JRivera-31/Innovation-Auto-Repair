5ac4ce59ff7794b8a65f8871eb080af049b63a9c
Emily Renegar <egrenegar@gmail.com>
2020-07-13 17:16:06 -0400

    re-made Employee Login form and added Layout component

--- components/EmployeeLoginForm/login.js
+++ components/EmployeeLoginForm/login.js
@@ -1,20 +1,19 @@
 import styles from "./login.module.css"
 
 export default function Form() {
-    return (
-        <div>
-            <form className={styles.form}>
-            <h1>Employee Login</h1>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Username</label>
-              <input type="username" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" />
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className={styles.formDiv}>
+      <div className={styles.heading}>
+        <h1>Employee Login</h1>
+        <hr className={styles.formHr}/>
+      </div>
+      <form className={styles.loginForm}>
+        <label className={styles.formLabel}>Username:</label>
+        <input className={styles.formInput} type="username" placeholder='Username'/>
+        <label className={styles.formLabel}>Password:</label>
+        <input className={styles.formInput} type="password" placeholder='Password'/>
+        <button className={styles.formSubmit} type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }

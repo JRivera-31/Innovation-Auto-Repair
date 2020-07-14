476ca241e9b4edde46f13ae804002c1cb1a4f475
Emily Renegar <egrenegar@gmail.com>
2020-07-14 14:30:22 -0400

    cleaned up code

--- components/DashboardTable/DashboardTable.js
+++ components/DashboardTable/DashboardTable.js
@@ -1,4 +1,4 @@
-import Table from "react-bootstrap/Table";
+import Table from 'react-bootstrap/Table';
 import style from './dashboard.module.css'
 import API from '../../util/API';
 import { useEffect, useState } from 'react';
@@ -11,34 +11,34 @@ const DashboardTable = () => {
     })
 
     return (
-        <div className={style.dashboardContainer}>
-      <Table responsive>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
+      <div className={style.dashboardContainer}>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
             {appointmentData.map((item, i) => {
-                return (
-                    <tr>
-                        <th>{i}</th>
-                        <th>{item.date.split(' ').join('/')}  {item.time.split(' ').join(':')}</th>
-                        <th>{item.name}</th>
-                        <th>{item.email}</th>
-                        <th>{item.phonenumber}</th>
-                        <th>{item.description}</th>
-                    </tr>
-                )
+              return (
+                <tr>
+                  <th>{i}</th>
+                  <th>{item.date.split(' ').join('/')}  {item.time.split(' ').join(':')}</th>
+                  <th>{item.name}</th>
+                  <th>{item.email}</th>
+                  <th>{item.phonenumber}</th>
+                  <th>{item.description}</th>
+                </tr>
+              )
             })}
-        </tbody>
-      </Table>
-    </div>
+          </tbody>
+        </Table>
+      </div>
     )
 }
 

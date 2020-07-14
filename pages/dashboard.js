00af3cb6f6a32a91171d82acd4ee55563cbaf2f1
Josh R <jriver35@uncc.edu>
2020-07-13 21:49:30 -0400

    added dashboard page to table

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,50 @@
+import Table from "react-bootstrap/Table";
+
 export default function Dashboard() {
-    return (
-        <div>
-            Yo
-        </div>
-    )
+  return (
+    <div>
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }

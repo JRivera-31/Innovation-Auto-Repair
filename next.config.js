fd35bb741874718e462b4105dcb655c7fcc4983a
Josh R <jriver35@uncc.edu>
2020-07-25 14:53:07 -0400

    config tweaks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+require("dotenv").config()
+
 module.exports = {
     env: {
         MONGO_URI: process.env.MONGODB_URI,
-        DB_NAME: process.env.DB_NAME
+        DB_NAME: process.env.DB_NAME,
+        WEB_URI: process.env.WEB_URI
     }
 }
